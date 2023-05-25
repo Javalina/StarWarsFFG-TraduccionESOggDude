df49659c3ad888d3d83b43adc79386b4ff471f63
eurbano-ci <hakuhimura@gmail.com>
2023-05-25 07:47:56 +0200

    Update register-babele.js

--- ffg-star-wars-traduction-fr-oggdude/register-babele.js
+++ ffg-star-wars-traduction-fr-oggdude/register-babele.js
@@ -1,8 +1,8 @@
 Hooks.on('init', () => {
     if(typeof Babele !== 'undefined') {
         Babele.get().register({
-            module: 'ffg-star-wars-traduction-fr-oggdude',
-            lang: 'fr',
+            module: 'ffg-star-wars-traduction-es-oggdude',
+            lang: 'es',
             dir: 'compendium'
         });
     }
